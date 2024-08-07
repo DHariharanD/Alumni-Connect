d5c19da79125ee0b97a7c4b63b219bb8a02b346f
dhanushgowdahm <dhanushhm1012@gmail.com>
2024-08-08 00:47:11 +0530

    resolved commit issues

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -1,42 +1,10 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
-=======
-// Assuming this is in your models.js or a similar file
-const mongoose = require('mongoose');
-
->>>>>>> d7d7588074994b7c7aa7dc1cee7c00a47ca67dd9
 const EventSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: Date, required: true },
-<<<<<<< HEAD
   confirmedusers: [{ type: String,}],
   
   incrementingValue: { type: Number, default: 0 }
 });
 module.exports = mongoose.model('Event', EventSchema);
-=======
-  attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  nonAttendees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-});
-
-// Method to add attendee
-EventSchema.methods.addAttendee = function(userId) {
-  this.attendees.push(userId);
-  return this.save();
-};
-
-// Method to remove attendee
-EventSchema.methods.removeAttendee = function(userId) {
-  const index = this.attendees.indexOf(userId);
-  if (index !== -1) {
-    this.attendees.splice(index, 1);
-  }
-  return this.save();
-};
-
-// Similar methods for nonAttendees can be added here
-
-module.exports = mongoose.model('Event', EventSchema);
-
->>>>>>> d7d7588074994b7c7aa7dc1cee7c00a47ca67dd9
